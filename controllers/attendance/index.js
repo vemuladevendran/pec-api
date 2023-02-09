3808bb34805acba087ed4a22274f84b789677b91
devendran <devendranvemula@gmail.com>
2023-02-09 10:53:43 +0530

    attendance report

--- controllers/attendance/index.js
+++ controllers/attendance/index.js
@@ -1,4 +1,4 @@
-const attendance = require('../../models/attendance');
+const Attendance = require('../../models/attendance');
 
 
 const markAttendance = async (req, res, next) => {
@@ -14,14 +14,14 @@ const markAttendance = async (req, res, next) => {
             section: req.body.section,
             periodNumber: req.body.periodNumber
         };
-        const data = await attendance.findOne(filters);
+        const data = await Attendance.findOne(filters);
         if (data) return res.status(400).json({ message: 'Attendance is already marked for this period' });
         let absentCount = 0;
         req.body.students.map((x) => {
             if (!x.attendance) absentCount += 1;
         });
         req.body.absentCount = absentCount;
-        await attendance.create(req.body);
+        await Attendance.create(req.body);
         return res.status(201).json({ message: `Attendance marked for ${req.body.periodNumber} period` })
     } catch (error) {
         console.log(error);
@@ -46,8 +46,51 @@ const getAttendanceReport = async (req, res, next) => {
             filters.year = req.query.year;
         }
 
-        const data = await attendance.find(filters);
+        const data = await Attendance.find(filters);
         return res.status(200).json({ data: data.sort((a, b) => (a.periodNumber > b.periodNumber ? 1 : -1)) })
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+
+// getting attendance report by numberof absent day count.
+const attendanceReports = async (req, res, next) => {
+    try {
+        const department = req?.user.department;
+        const { from, to, count, year } = req.query;
+        if (!from || !to) return res.status(400).json({ message: 'provide correct from and to date' })
+        const absentsList = await Attendance.aggregate([
+            {
+                $match: {
+                    date: { $gte: new Date(new Date(from).setHours(0, 0, 0, 0)), $lte: new Date(new Date(to).setHours(23, 59, 59, 999)) },
+                    departmentName: department,
+                    year: year,
+                }
+            },
+            {
+                $unwind: "$students"
+            },
+            {
+                $group: {
+                    _id: "$students.examNumber",
+                    exmaNumber: { $first: "$students.examNumber" },
+                    totalAbsentCount: {
+                        $sum: {
+                            $cond: ["$students.attendance", 0, 1]
+                        }
+                    }
+                }
+            },
+            {
+                $match: {
+                    totalAbsentCount: { $gte: parseInt(count) || 0 },
+                }
+            }
+        ]);
+        const data = absentsList.sort((a, b) => (a.totalAbsentCount > b.totalAbsentCount ? -1 : 1))
+        return res.status(200).json(data);
     } catch (error) {
         console.log(error);
         next(error);
@@ -59,5 +102,6 @@ const getAttendanceReport = async (req, res, next) => {
 
 module.exports = {
     markAttendance,
-    getAttendanceReport
+    getAttendanceReport,
+    attendanceReports
 }
--- routes/attendance.js
+++ routes/attendance.js
@@ -1,7 +1,8 @@
-const { markAttendance, getAttendanceReport } = require('../controllers/attendance/index');
-
+const { markAttendance, getAttendanceReport, attendanceReports } = require('../controllers/attendance/index');
+const { verifyToken } = require("../services/auth")
 
 module.exports = function attendanceRoutes(app) {
   app.post("/api/v1/attendance", markAttendance);
   app.get("/api/v1/attendance", getAttendanceReport);
+  app.get("/api/v1/attendance-reports", verifyToken, attendanceReports);
 };
