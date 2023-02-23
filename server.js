52f11bb1b4a7bf2513654d72399c469bbcc249be
devendran <devendranvemula@gmail.com>
2023-02-23 10:48:07 +0530

    cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const morgan = require("morgan");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
-app.use(cors({ origin: '*' }))
+// app.use(cors({ origin: '*' }))
 
 app.use("/static", express.static("uploads"));
 
