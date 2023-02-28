decc9a1b9559d55149d6245a77e4a301d16a09c0
devendran <devendranvemula@gmail.com>
2023-02-28 15:51:00 +0530

    clean

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,9 @@ const {
     resetStaffPassword,
 } = require("../controllers/auth/index");
 
+const { checkToken } = require("../services/auth");
+
+
 module.exports = function authRoutes(app) {
     app.post("/api/v1/admin/login", adminLogin);
     app.post("/api/v1/teacher/checkemail", checkTeacherEmail);
@@ -18,6 +21,6 @@ module.exports = function authRoutes(app) {
     app.post("/api/v1/student/checkemail", checkStudentEmail);
     app.post("/api/v1/student/login", studentLogin);
     app.post("/api/v1/student/setpassword", setStudentPassword);
-    app.get("/api/v1/reset-password/student", resetStudentPassword);
-    app.get("/api/v1/reset-password/teacher", resetStaffPassword);
+    app.get("/api/v1/reset-password/student", checkToken, resetStudentPassword);
+    app.get("/api/v1/reset-password/teacher", checkToken, resetStaffPassword);
 };
